7b35b51b2d0d10b90d195503881b4b32d262d472
B1A2 <B1A2@noreply.gitcode.com>
2025-05-08 14:16:11 +0800

    增加京东测试用例0020，0030，0080，0090，0120

---
+++ perf_testing/testcases/com.jd.hm.mall/ResourceUsage_PerformanceDynamic_jingdong_0120.py
@@ -0,0 +1,71 @@
+# coding: utf-8
+import os
+import time
+
+from devicetest.core.test_case import Step
+from hypium import BY
+
+from aw.PerfTestCase import PerfTestCase, Log
+from aw.common.CommonUtils import CommonUtils
+
+
+class ResourceUsage_PerformanceDynamic_jingdong_0120(PerfTestCase):
+
+    def __init__(self, controllers):
+        self.TAG = self.__class__.__name__
+        super().__init__(self.TAG, controllers)
+
+        self._app_package = 'com.jd.hm.mall'
+        self._app_name = '京东'
+        self._steps = [
+            {
+                "name": "step1",
+                "description": "1.京东9.9包邮场景，进入9.9页面，等3s，上滑3次，下滑3次，每次停留2s"
+            }
+        ]
+
+    @property
+    def steps(self) -> []:
+        return self._steps
+
+    @property
+    def app_package(self) -> str:
+        return self._app_package
+
+    @property
+    def app_name(self) -> str:
+        return self._app_name
+
+    def setup(self):
+        Log.info('setup')
+        os.makedirs(os.path.join(self.report_path, 'hiperf'), exist_ok=True)
+        os.makedirs(os.path.join(self.report_path, 'report'), exist_ok=True)
+
+    def process(self):
+        self.driver.swipe_to_home()
+
+        Step('启动京东应用')
+        self.driver.start_app(self.app_package)
+        self.driver.wait(5)
+
+
+
+        def step1(driver):
+            # 点击9.9包邮
+            self.driver.touch((904, 1233))
+            time.sleep(3)
+
+            Step('上滑操作')
+            CommonUtils.swipes_up_load(self.driver, swip_num=3, sleep=2)
+            Step('下滑操作')
+            CommonUtils.swipes_down_load(self.driver, swip_num=5, sleep=2)
+
+
+        self.execute_step_with_perf(1, step1, 30)
+
+
+
+    def teardown(self):
+        Log.info('teardown')
+        self.driver.stop_app(self.app_package)
+        self.make_reports()
