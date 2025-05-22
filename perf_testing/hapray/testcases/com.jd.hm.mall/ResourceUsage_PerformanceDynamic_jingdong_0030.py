5d69f208bffc3772c16491e61cde66f212556ed5
han hu <543160303@qq.com>
2025-05-22 15:53:52 +0800

    适配京东样例0030到京东app 2025.5.22 版本

--- perf_testing/hapray/testcases/com.jd.hm.mall/ResourceUsage_PerformanceDynamic_jingdong_0030.py
+++ perf_testing/hapray/testcases/com.jd.hm.mall/ResourceUsage_PerformanceDynamic_jingdong_0030.py
@@ -24,9 +24,10 @@ class ResourceUsage_PerformanceDynamic_jingdong_0030(PerfTestCase):
             }
         ]
         
-        # 原始采集设备的屏幕尺寸（Mate 60 Pro）
-        self.source_screen_width = 1212
-        self.source_screen_height = 2616
+        # 原始采集设备的屏幕尺寸（Mate 60）
+        self.source_screen_width = 1216
+        self.source_screen_height = 2688
+
 
     @property
     def steps(self) -> []:
@@ -55,6 +56,29 @@ class ResourceUsage_PerformanceDynamic_jingdong_0030(PerfTestCase):
 
         def step1(driver):
             # 点击京东超市
+            # 从(824, 922)滑动至(7, 1344)
+            p1 = CoordinateAdapter.convert_coordinate(
+                self.driver,
+                x=824,  # 原始x坐标
+                y=922,  # 原始y坐标
+                source_width=self.source_screen_width,
+                source_height=self.source_screen_height
+            )
+            p2 = CoordinateAdapter.convert_coordinate(
+                self.driver,
+                x=7,  # 原始x坐标
+                y=1344,  # 原始y坐标
+                source_width=self.source_screen_width,
+                source_height=self.source_screen_height
+            )
+            self.driver.slide(p1, p2)
+            self.driver.wait(1)
+            # 点击type为{Text}并且text为{全部频道}的控件
+            self.driver.touch(BY.type('Text').text('全部频道'))
+            self.driver.wait(2)
+
+            CommonUtils.swipes_up_load(self.driver, swip_num=1, sleep=2)
+
             self.driver.touch(BY.text('京东超市'))
             self.driver.wait(2)
 
@@ -63,15 +87,15 @@ class ResourceUsage_PerformanceDynamic_jingdong_0030(PerfTestCase):
             self.driver.wait(2)
 
             # Step('粮油调味页上滑操作')
-            CommonUtils.swipes_up_load(self.driver, swip_num=3, sleep=2)
+            CommonUtils.swipes_up_load(self.driver, swip_num=2, sleep=2)
             # Step('粮油调味页下滑操作')
-            CommonUtils.swipes_down_load(self.driver, swip_num=5, sleep=2)
+            CommonUtils.swipes_down_load(self.driver, swip_num=6, sleep=2)
 
             # 加入第一个商品到购物车
             self.driver.touch(CoordinateAdapter.convert_coordinate(
                 self.driver,
-                x=1160,  # 原始x坐标
-                y=1167,  # 原始y坐标
+                x=1124,  # 原始x坐标
+                y=1116,  # 原始y坐标
                 source_width=self.source_screen_width,
                 source_height=self.source_screen_height
             ))
@@ -82,8 +106,8 @@ class ResourceUsage_PerformanceDynamic_jingdong_0030(PerfTestCase):
         # 从购物车移除第一个商品
         self.driver.touch(CoordinateAdapter.convert_coordinate(
             self.driver,
-            x=1005,  # 原始x坐标
-            y=1167,  # 原始y坐标
+            x=972,  # 原始x坐标
+            y=1116,  # 原始y坐标
             source_width=self.source_screen_width,
             source_height=self.source_screen_height
         ))
