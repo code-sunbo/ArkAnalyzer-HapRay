403b1d333b7453d430b846203cb45ad07c1ccc8d
B1A2 <B1A2@noreply.gitcode.com>
2025-06-06 19:55:17 +0800

    rom信息解耦，去掉轮次信息

--- toolbox/src/cli/commands/hapray_cli.ts
+++ toolbox/src/cli/commands/hapray_cli.ts
@@ -55,16 +55,6 @@ export interface TestInfo {
     timestamp: number;
 }
 
-export interface SummaryInfo {
-    rom_version: string,
-    app_version: string,
-    scene: string,
-    step_name: string,
-    step_id: number,
-    round: number,
-    count: number,
-}
-
 export interface ResultInfo {
     rom_version: string;
     device_sn: string;
@@ -100,67 +90,18 @@ async function main(input: string): Promise<void> {
             fs.mkdirSync(output, { recursive: true });
         }
 
-        let resultInfo: ResultInfo = { rom_version: '', device_sn: '' };
-
-        //load result.xml
-        let resultXml = path.join(roundFolders[0], 'result', path.basename(input) + '.xml');
-        if (!fs.existsSync(resultXml)) {
-            logger.error('load' + resultXml + ':失败！');
-        } else {
-            try {
-                // 使用 DOMParser 解析 XML字符串
-                const parser = new DOMParser();
-                const xmlDoc = parser.parseFromString(fs.readFileSync(resultXml, 'utf-8'), 'text/xml');
-
-                // 获取 testsuites 元素
-                const testsuitesElement = xmlDoc.getElementsByTagName('testsuites')[0];
-
-                // 提取starttime 属性
-                let devicesAttr = testsuitesElement.getAttribute('devices');
-
-                if (!devicesAttr) {
-                    logger.error('parse' + resultXml + ':失败！');
-                } else {
-                    const devices = JSON.parse(
-                        devicesAttr
-                            .replace(/<!\[CDATA\[(.*?)\]\]>/gs, '$1')
-                            .replace(/^\[+/, '')
-                            .replace(/]+$/, '')
-                            .replace(/\\"/g, '"')
-                            .replace(/'/g, '"'));
-                    resultInfo.rom_version = devices.version;
-                    resultInfo.device_sn = devices.sn;
-                }
-            } catch (error) {
-                logger.error('parse' + resultXml + ':失败！');
-            }
-        }
-
         // load testinfo.json
         let testInfoPath = path.join(roundFolders[0], 'testInfo.json')
         await copyFile(testInfoPath, path.join(input, 'testInfo.json'));
-        let rawData = fs.readFileSync(testInfoPath, 'utf8');
-        const testInfo: TestInfo = JSON.parse(rawData);
 
         // load steps.json
         let stepsJsonPath = path.join(roundFolders[0], 'hiperf', 'steps.json')
         await copyFile(stepsJsonPath, path.join(input, 'hiperf', 'steps.json'));
-        rawData = fs.readFileSync(stepsJsonPath, 'utf8');
+        let rawData = fs.readFileSync(stepsJsonPath, 'utf8');
         const steps: Steps = JSON.parse(rawData);
-        let stepsCollect: StepJsonData[] = [];
         for (let i = 0; i < steps.length; i++) {
-            let stepItem: StepJsonData = {
-                step_name: steps[i].description,
-                step_id: steps[i].stepIdx,
-                count: 0,
-                round: 0,
-                perf_data_path: '',
-                data: []
-            };
             let result: number[] = [];
             let perfAnalyzer = new PerfAnalyzer('');
-            let dbPaths: string[] = [];
-            let tracePaths: string[] = [];
             let choose = 0;
             let tracePath = '';
             let dbPath = '';
@@ -173,8 +114,6 @@ async function main(input: string): Promise<void> {
                     if (!fs.existsSync(dbPath)) {
                         traceStreamerCmd(tracePath, dbPath);
                     }
-                    dbPaths.push(dbPath);
-                    tracePaths.push(tracePath);
 
                     const sum = await perfAnalyzer.calcPerfDbTotalInstruction(dbPath);
                     result[index] = sum;
@@ -213,12 +152,9 @@ async function main(input: string): Promise<void> {
                 if (!fs.existsSync(dbPath)) {
                     traceStreamerCmd(tracePath, dbPath);
                 }
-                dbPaths.push(dbPath);
-                tracePaths.push(tracePath);
-                result.push(await perfAnalyzer.calcPerfDbTotalInstruction(dbPath));
             }
 
-            logger.info(dbPaths[choose] + ' : setp' + i + ' select round' + choose + ' .');
+            logger.info(roundFolders[choose] + ' : setp' + i + ' select round' + choose + ' .');
             //将hiperf和htrace文件复制到最终目录
             const choosePerfDir = path.join(roundFolders[choose], 'hiperf', `step${steps[i].stepIdx.toString()}`);
             const chooseHtraceDir = path.join(roundFolders[choose], 'htrace', `step${steps[i].stepIdx.toString()}`);
@@ -229,30 +165,49 @@ async function main(input: string): Promise<void> {
             await copyDirectory(choosePerfDir, scenePerfDir);
             await copyDirectory(chooseHtraceDir, sceneHtraceDir);
             await copyDirectory(chooseResultDir, ResultDir);
-            stepItem.count = result[choose];
-            stepItem.round = choose;
-            stepItem.perf_data_path = tracePaths[choose];
-            stepsCollect.push(stepItem);
         }
 
-        await saveJsonFile(output, resultInfo, testInfo, stepsCollect);
     } else {//输出hiperfjson
         logger.info(`Input dir is: ${input}`);
 
         let output = path.join(input, 'report');
-        let summaryJsonPath = path.join(output, 'summary_info.json');
-        if (!fs.existsSync(summaryJsonPath)) {
-            logger.error(`${summaryJsonPath}: 文件不存在，说明轮次选择步骤失败，需要重试上一步！`);
-            return;
+
+        let resultInfo: ResultInfo = { rom_version: '', device_sn: '' };
+
+        //load result.xml
+        let resultXml = path.join(input, 'result', path.basename(input) + '.xml');
+        if (!fs.existsSync(resultXml)) {
+            logger.error('load' + resultXml + ':失败！');
+        } else {
+            try {
+                // 使用 DOMParser 解析 XML字符串
+                const parser = new DOMParser();
+                const xmlDoc = parser.parseFromString(fs.readFileSync(resultXml, 'utf-8'), 'text/xml');
+
+                // 获取 testsuites 元素
+                const testsuitesElement = xmlDoc.getElementsByTagName('testsuites')[0];
+
+                // 提取starttime 属性
+                let devicesAttr = testsuitesElement.getAttribute('devices');
+
+                if (!devicesAttr) {
+                    logger.error('parse' + resultXml + ':失败！');
+                } else {
+                    const devices = JSON.parse(
+                        devicesAttr
+                            .replace(/<!\[CDATA\[(.*?)\]\]>/gs, '$1')
+                            .replace(/^\[+/, '')
+                            .replace(/]+$/, '')
+                            .replace(/\\"/g, '"')
+                            .replace(/'/g, '"'));
+                    resultInfo.rom_version = devices.version;
+                    resultInfo.device_sn = devices.sn;
+                }
+            } catch (error) {
+                logger.error('parse' + resultXml + ':失败！');
+            }
         }
-        //load summary_info.json
-        let summaryJson = fs.readFileSync(summaryJsonPath, 'utf8');
-        const summaryJsons: SummaryInfo[] = JSON.parse(summaryJson);
-        let checkResult = await checkPerfAndHtraceFiles(input,summaryJsons.length);
-        if(!checkResult){
-            logger.error('hiperf数据或者htrace数据不全，需要执行上一步补充！');
-            return;
-        }
+        
         // load testinfo.json
         let testInfoPath = path.join(input, 'testInfo.json')
         let rawData = fs.readFileSync(testInfoPath, 'utf8');
@@ -262,6 +217,11 @@ async function main(input: string): Promise<void> {
         let stepsJsonPath = path.join(input, 'hiperf', 'steps.json')
         rawData = fs.readFileSync(stepsJsonPath, 'utf8');
         const steps: Steps = JSON.parse(rawData);
+        let checkResult = await checkPerfAndHtraceFiles(input, steps.length);
+        if (!checkResult) {
+            logger.error('hiperf数据或者htrace数据不全，需要执行上一步补充！');
+            return;
+        }
         let perfDataPaths = getPerfDataPaths(input, steps);
         let perfDbPaths = getPerfDbPaths(input, steps);
         let htracePaths = getHtracePaths(input, steps);
@@ -271,38 +231,21 @@ async function main(input: string): Promise<void> {
             let stepItem: StepJsonData;
             let perfAnalyzer = new PerfAnalyzer('');
             stepItem = await perfAnalyzer.analyze2(perfDbPaths[i], testInfo.app_id, steps[i]);
-            let testSceneInfo: TestSceneInfo = { packageName: testInfo.app_id, scene: testInfo.scene, osVersion: summaryJsons[i].rom_version, timestamp: testInfo.timestamp };
+            let testSceneInfo: TestSceneInfo = { packageName: testInfo.app_id, scene: testInfo.scene, osVersion: resultInfo.rom_version, timestamp: testInfo.timestamp };
             await perfAnalyzer.analyze(perfDbPaths[i], testSceneInfo, output, steps[i].stepIdx);
-            stepItem.round = summaryJsons[i].round;
+            stepItem.round = 0;
             stepItem.perf_data_path = perfDbPaths[i];
             stepsCollect.push(stepItem);
         }
-        await saveHiperfJson(output, summaryJsons[0], testInfo, perfDataPaths, perfDbPaths, htracePaths, stepsCollect);
+        await saveHiperfJson(output, resultInfo, testInfo, perfDataPaths, perfDbPaths, htracePaths, stepsCollect);
     }
 
 }
 
-async function saveJsonFile(output: string, resultInfo: ResultInfo, testInfo: TestInfo, steps: StepJsonData[]): Promise<void> {
-    let summaryJsonObject: SummaryInfo[] = []
-    steps.forEach(step => {
-        const summaryObject: SummaryInfo = {
-            rom_version: resultInfo.rom_version,
-            app_version: testInfo.app_version,
-            scene: testInfo.scene,
-            step_name: step.step_name,
-            step_id: step.step_id,
-            round: step.round,
-            count: step.count
-        }
-        summaryJsonObject.push(summaryObject);
-    })
-    await saveJsonArray(summaryJsonObject, path.join(output, 'summary_info.json'));
-}
-
-async function saveHiperfJson(output: string, summaryInfo: SummaryInfo, testInfo: TestInfo, perfDataPaths: string[], perfDbPaths: string[], htracePaths: string[], steps: StepJsonData[]): Promise<void> {
-    output = path.join(output,'../','hiperf');
+async function saveHiperfJson(output: string, resultInfo: ResultInfo, testInfo: TestInfo, perfDataPaths: string[], perfDbPaths: string[], htracePaths: string[], steps: StepJsonData[]): Promise<void> {
+    output = path.join(output, '../', 'hiperf');
     const jsonObject = {
-        rom_version: summaryInfo.rom_version,
+        rom_version: resultInfo.rom_version,
         app_id: testInfo.app_id,
         app_name: testInfo.app_name,
         app_version: testInfo.app_version,
